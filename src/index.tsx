81c88a178d4a456484d0733abad1a2de2fcdf0df
Steam Deck User <deck@steamdeck.lan>
2023-05-27 22:16:01 +0800

    Try making a valid plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import {
+  Field,
+  Focusable,
   ButtonItem,
   definePlugin,
   DialogButton,
@@ -11,95 +13,87 @@ import {
   showContextMenu,
   staticClasses,
 } from "decky-frontend-lib";
-import { VFC } from "react";
-import { FaShip } from "react-icons/fa";
 
-import logo from "../assets/logo.png";
+import { VFC, ReactNode } from "react";
+import { BsTerminalFill, BsLightningFill, BsGrid3X3GapFill } from "react-icons/bs";
 
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
+const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
+  const spawnProcess = async (cmd) => {
+    const result = await serverAPI.callPluginMethod("launch", { "cmd": cmd });
+    if (result.success) {
+      serverAPI.toaster.toast({
+          "title": "Executed " + (result.result.retcode == 0 ? "successfully" : "with error " + result.result.retcode),
+          "body": (result.result.stdout + " " + result.result.stderr)
+      });
+    }
+  };
 
-const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
-  // const [result, setResult] = useState<number | undefined>();
+  const LaunchMenu : VFC<{name : string, cmd : string}> = ({name, cmd}) => {
+      return (<MenuItem onSelected={async () => {await spawnProcess(cmd);}}>{name}</MenuItem>);
+  }
 
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
+  const LaunchRow : VFC<{name : string, cmd : string, children?: ReactNode}> = ({name, cmd, children}) => {
+    const showCtxMenu = (e: MouseEvent | GamepadEvent) => {
+      showContextMenu(
+        <Menu label={name}>
+          {children}
+        </Menu>,
+        e.currentTarget ?? window,
+      );
+    };
+
+    const button_style = {
+      minHeight: "35px",
+      minWidth: "40px",
+      maxWidth: "40px",
+      paddingLeft: "0px",
+      paddingRight: "0px",
+      justifyContent: "center",
+      alignItems: "center"
+    };
+
+    const margin_right_style = {
+      marginRight: "2px",
+    };
+
+    return (
+      <Field label={name}>
+        <Focusable style={{display: 'flex', flexDirection: 'row', paddingLeft: "10px", paddingRight: "10px"}}>
+          <DialogButton
+            style={{...button_style, ...margin_right_style}}
+            onClick={async() => {await spawnProcess(cmd);}}>
+            <BsLightningFill/>
+          </DialogButton>
+          <DialogButton
+            style={button_style}
+            onClick={(e) => { showCtxMenu(e); }}>
+            <BsGrid3X3GapFill/>
+          </DialogButton>
+        </Focusable>
+      </Field>
+    );
+  };
 
   return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
-        >
-          Server says yolo
-        </ButtonItem>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Router.CloseSideMenus();
-            Router.Navigate("/decky-plugin-test");
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>
+    <PanelSection title="Services">
+      <LaunchRow name="Leaf" cmd="/home/deck/Dotfiles/deck/leaf.sh start">
+        <LaunchMenu name="Status" cmd="/home/deck/Dotfiles/deck/leaf.sh"/>
+        <LaunchMenu name="Start Global" cmd="/home/deck/Dotfiles/deck/leaf.sh start global"/>
+        <LaunchMenu name="Stop" cmd="/home/deck/Dotfiles/deck/leaf.sh stop"/>
+      </LaunchRow>
+      <LaunchRow name="Auto Brightness" cmd="XDG_RUNTIME_DIR=/run/user/1000 systemctl --user restart auto_brightness">
+        <LaunchMenu name="Status" cmd="XDG_RUNTIME_DIR=/run/user/1000 systemctl --user is-active auto_brightness"/>
+        <LaunchMenu name="Stop" cmd="XDG_RUNTIME_DIR=/run/user/1000 systemctl --user stop auto_brightness"/>
+      </LaunchRow>
     </PanelSection>
   );
 };
 
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Router.NavigateToLibraryTab()}>
-        Go to Library
-      </DialogButton>
-    </div>
-  );
-};
-
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
-
   return {
-    title: <div className={staticClasses.Title}>Example Plugin</div>,
+    title: <div className={staticClasses.Title}>Launcher</div>,
     content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
-    onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
-    },
+    icon: <BsTerminalFill />,
+    onDismount() {},
   };
 });
